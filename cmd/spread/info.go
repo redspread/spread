595a62c8c806068e2fa38797d29d8f19da782c42
Dan Gillespie <dan@redspread.com>
2016-01-14 17:35:05 -0800

    Basic CLI setup

---
+++ cmd/spread/info.go
@@ -0,0 +1,9 @@
+package main
+
+const (
+	// Usage is a short explanation of what spread does
+	Usage = "Quickly iterate with Kubernetes"
+
+	// Version is the current version number
+	Version = "0.0.1"
+)
---
+++ cmd/spread/spread.go
@@ -0,0 +1,43 @@
+package main // import "rsprd.com/spread/cmd/spread"
+
+import (
+	"os"
+	"reflect"
+
+	"rsprd.com/spread/cli"
+
+	clilib "github.com/codegangsta/cli"
+)
+
+func main() {
+	spread := cli.NewSpreadCli(os.Stdin, os.Stdout, os.Stderr, Version)
+
+	app := app()
+	app.Commands = commands(spread)
+	app.Run(os.Args)
+}
+
+// Get commands provided by spread
+func commands(spread *cli.SpreadCli) []clilib.Command {
+	cmds := []clilib.Command{}
+
+	cliType := reflect.ValueOf(spread)
+	for i := 0; i < cliType.NumMethod(); i++ {
+		cmd := cliType.Method(i)
+
+		// check that is returning command
+		cmdType := cmd.Type()
+		if cmdType.NumOut() == 1 && cmdType.Out(0) == reflect.TypeOf(clilib.Command{}) {
+			command := cmd.Interface().(func() clilib.Command)
+			cmds = append(cmds, command())
+		}
+	}
+	return cmds
+}
+
+func app() *clilib.App {
+	app := clilib.NewApp()
+	app.Usage = Usage
+	app.Version = Version
+	return app
+}
