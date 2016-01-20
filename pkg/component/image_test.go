f50cf075246fbfaf52ff3e33ed0e2ed744235dcc
Dan Gillespie <dan@redspread.com>
2016-01-19 21:36:27 -0800

    added more image tests

--- pkg/component/image_test.go
+++ pkg/component/image_test.go
@@ -6,16 +6,14 @@ import (
 
 	"rsprd.com/spread/pkg/image"
 
+	"github.com/stretchr/testify/assert"
 	"k8s.io/kubernetes/pkg/api"
 	"rsprd.com/spread/pkg/deploy"
 )
 
 func TestNewImage(t *testing.T) {
 	imageName := "gcr.io/google_containers/cassandra:v7"
-	simple, err := image.FromString(imageName)
-	if err != nil {
-		t.Fatalf("Could not create image.Image: %v", err)
-	}
+	simple := newImage(t, imageName)
 
 	secret := api.Secret{
 		ObjectMeta: api.ObjectMeta{
@@ -30,6 +28,11 @@ func TestNewImage(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Could not create Image component: %v", err)
 	}
+
+	// check images
+	images := image.Images()
+	assert.Len(t, images, 1, "supposed to be single image")
+	assert.EqualValues(t, simple, images[0], "should return image it was created with")
 
 	expectedPod := api.Pod{
 		Spec: api.PodSpec{
@@ -50,4 +53,31 @@ func TestNewImage(t *testing.T) {
 	if !reflect.DeepEqual(expected, actual) {
 		t.Errorf("Expected: %#v, saw: %#v", expected, actual)
 	}
+}
+
+func TestNilImage(t *testing.T) {
+	var image *image.Image
+	_, err := NewImage(image, "")
+	if err == nil {
+		t.Errorf("Nil should return an error.")
+	}
+}
+
+func TestImageType(t *testing.T) {
+	image := newImage(t, "ghost:latest")
+
+	component, err := NewImage(image, "")
+	if err != nil {
+		t.Fatalf("Could not create Image component: %v", err)
+	}
+
+	assert.Equal(t, ComponentImage, component.Type(), "incorrect type")
+}
+
+func newImage(t *testing.T, imageName string) *image.Image {
+	simple, err := image.FromString(imageName)
+	if err != nil {
+		t.Fatalf("Could not create image.Image: %v", err)
+	}
+	return simple
 }
