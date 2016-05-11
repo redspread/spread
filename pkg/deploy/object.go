1763605b0fe1c9d13c9f4d6bdb08b6849efcf364
Dan Gillespie <dan@redspread.com>
2016-05-11 11:24:52 -0700

    added getting full path from objects

--- pkg/deploy/object.go
+++ pkg/deploy/object.go
@@ -1,6 +1,10 @@
 package deploy
 
 import (
+	"errors"
+	"fmt"
+
+	kube "k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/api/meta"
 	"k8s.io/kubernetes/pkg/runtime"
 )
@@ -9,4 +13,21 @@ import (
 type KubeObject interface {
 	meta.ObjectMetaAccessor
 	runtime.Object
+}
+
+// FullObjectPath returns the full path of an object.
+// This uses the format "<apiVersion>/namespaces/<namespace>/<kind>/<name>"
+func FullObjectPath(obj KubeObject) (string, error) {
+	errText := "could not get object path"
+
+	// attempt to determine ObjectKind
+	gkv, err := kube.Scheme.ObjectKind(obj)
+	if err != nil {
+		return "", fmt.Errorf(errText+": %v", err)
+	} else if gkv.IsEmpty() {
+		return "", errors.New(errText + ": empty ObjectKind")
+	}
+
+	meta := obj.GetObjectMeta()
+	return fmt.Sprintf("%s/namespaces/%s/%s/%s", obj.GetObjectKind().GroupVersionKind().Version, meta.GetNamespace(), gkv.Kind, meta.GetName()), nil
 }
---
+++ pkg/deploy/object_test.go
@@ -0,0 +1,42 @@
+package deploy
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	kube "k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/api/unversioned"
+)
+
+func TestObjectPath(t *testing.T) {
+	rc := &kube.ReplicationController{
+		TypeMeta: unversioned.TypeMeta{
+			APIVersion: "v1",
+		},
+		ObjectMeta: kube.ObjectMeta{
+			Name:      "johnson",
+			Namespace: kube.NamespaceDefault,
+		},
+	}
+
+	expected := "v1/namespaces/default/ReplicationController/johnson"
+	actual, err := FullObjectPath(rc)
+	assert.NoError(t, err)
+	assert.Equal(t, expected, actual)
+}
+
+func TestObjectPathNoNamespace(t *testing.T) {
+	rc := &kube.ReplicationController{
+		TypeMeta: unversioned.TypeMeta{
+			APIVersion: "v1",
+		},
+		ObjectMeta: kube.ObjectMeta{
+			Name: "johnson",
+		},
+	}
+
+	expected := "v1/namespaces//ReplicationController/johnson"
+	actual, err := FullObjectPath(rc)
+	assert.NoError(t, err)
+	assert.Equal(t, expected, actual)
+}
